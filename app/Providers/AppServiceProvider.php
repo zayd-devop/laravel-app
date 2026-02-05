42b1a56106966eebb68f587b59e40aa2f6988961
zaid <saousaouzaid10@gmail.com>
2026-02-05 12:11:21 +0100

    Fix Length AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // <--- 1. ADD THIS LINE
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191); // <--- 2. ADD THIS LINE
     }
 }
